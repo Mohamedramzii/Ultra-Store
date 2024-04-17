d52787b19fe21b82b6f044f13bcd920b660d9521
MohamedRamzii <mohamedramzii224@gmail.com>
2024-04-17 22:06:21 +0200

    core packages

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,22 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'full_ecommerce_app.dart';
+import 'package:full_ecommerce_app/full_ecommerce_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Future.wait([
-    Platform.isAndroid
-        ? Firebase.initializeApp(
-            options: const FirebaseOptions(
-                apiKey: 'AIzaSyBAz5OXsFADMdU-G_rD4_PENdLIYv0i7pU',
-                appId: '1:213592688170:android:0014413b30b33eb024a553',
-                messagingSenderId: '213592688170',
-                projectId: 'ultra-store-86c05'))
-        : Firebase.initializeApp()
+    if (Platform.isAndroid)
+      Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: 'AIzaSyBAz5OXsFADMdU-G_rD4_PENdLIYv0i7pU',
+          appId: '1:213592688170:android:0014413b30b33eb024a553',
+          messagingSenderId: '213592688170',
+          projectId: 'ultra-store-86c05',
+        ),
+      )
+    else
+      Firebase.initializeApp(),
   ]);
   runApp(const MyApp());
 }
